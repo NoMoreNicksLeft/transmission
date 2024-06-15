NoMoreNicksLeft/transmission

commit 726c93208ab0c47c267b9baaf4b8f43759d7a1d7

feat: add tr_strvToBuf() (#3657)
* feat: add tr_getDefaultConfigDirToBuf()

* feat: add tr_getDefaultDownloadDirToBuf()

* feat: add tr_strvToBuf()

* feat: add tr_torrentFindFileToBuf()

* feat: add tr_torrentGetMagnetLinkToBuf()

* refactor: remove unused makeString() from tests

* feat: add tr_torrentFilenameToBuf()

* feat: add tr_torrentGetTrackerListToBuf()

* chore: remove obsolete comment references to tr_free

* chore: remove unused tr_strdup()

* chore: remove unused forward declarations

cli/cli.cc
@@ -173,10 +173,9 @@ static std::string getStatusStr(tr_stat const* st)
     return "";
 }
 
-static char const* getConfigDir(int argc, char const** argv)
+static std::string getConfigDir(int argc, char const** argv)
 {
     int c;
-    char const* configDir = nullptr;
     char const* my_optarg;
     int const ind = tr_optind;
 
@@ -184,19 +183,14 @@ static char const* getConfigDir(int argc, char const** argv)
     {
         if (c == 'g')
         {
-            configDir = my_optarg;
+            return my_optarg;
             break;
         }
     }
 
     tr_optind = ind;
 
-    if (configDir == nullptr)
-    {
-        configDir = tr_getDefaultConfigDir(MyConfigName);
-    }
-
-    return configDir;
+    return tr_getDefaultConfigDir(MyConfigName);
 }
 
 int tr_main(int argc, char* argv[])
@@ -218,8 +212,8 @@ int tr_main(int argc, char* argv[])
 
     /* load the defaults from config file + libtransmission defaults */
     tr_variantInitDict(&settings, 0);
-    char const* const configDir = getConfigDir(argc, (char const**)argv);
-    tr_sessionLoadSettings(&settings, configDir, MyConfigName);
+    auto const config_dir = getConfigDir(argc, (char const**)argv);
+    tr_sessionLoadSettings(&settings, config_dir.c_str(), MyConfigName);
 
     /* the command line overrides defaults */
     if (parseCommandLine(&settings, argc, (char const**)argv) != 0)
@@ -256,7 +250,7 @@ int tr_main(int argc, char* argv[])
         }
     }
 
-    auto* const h = tr_sessionInit(configDir, false, &settings);
+    auto* const h = tr_sessionInit(config_dir.c_str(), false, &settings);
     auto* const ctor = tr_ctorNew(h);
 
     tr_ctorSetPaused(ctor, TR_FORCE, false);
@@ -353,7 +347,7 @@ int tr_main(int argc, char* argv[])
         }
     }
 
-    tr_sessionSaveSettings(h, configDir, &settings);
+    tr_sessionSaveSettings(h, config_dir.c_str(), &settings);
 
     printf("\n");
     tr_variantFree(&settings);

daemon/daemon.cc
@@ -202,10 +202,9 @@ static bool reopen_log_file(char const* filename)
     return true;
 }
 
-static char const* getConfigDir(int argc, char const* const* argv)
+static std::string getConfigDir(int argc, char const* const* argv)
 {
     int c;
-    char const* configDir = nullptr;
     char const* optstr;
     int const ind = tr_optind;
 
@@ -213,19 +212,13 @@ static char const* getConfigDir(int argc, char const* const* argv)
     {
         if (c == 'g')
         {
-            configDir = optstr;
-            break;
+            return optstr;
         }
     }
 
     tr_optind = ind;
 
-    if (configDir == nullptr)
-    {
-        configDir = tr_getDefaultConfigDir(MyName);
-    }
-
-    return configDir;
+    return tr_getDefaultConfigDir(MyName);
 }
 
 static auto onFileAdded(tr_session* session, std::string_view dirname, std::string_view basename)
@@ -671,7 +664,7 @@ static bool parse_args(
 struct daemon_data
 {
     tr_variant settings;
-    char const* configDir;
+    std::string config_dir;
     bool paused;
 };
 
@@ -719,7 +712,7 @@ static int daemon_start(void* varg, [[maybe_unused]] bool foreground)
 
     auto* arg = static_cast<daemon_data*>(varg);
     tr_variant* const settings = &arg->settings;
-    char const* const configDir = arg->configDir;
+    char const* const config_dir = arg->config_dir.c_str();
 
     sd_notifyf(0, "MAINPID=%d\n", (int)getpid());
 
@@ -744,10 +737,10 @@ static int daemon_start(void* varg, [[maybe_unused]] bool foreground)
     tr_formatter_mem_init(MemK, MemKStr, MemMStr, MemGStr, MemTStr);
     tr_formatter_size_init(DiskK, DiskKStr, DiskMStr, DiskGStr, DiskTStr);
     tr_formatter_speed_init(SpeedK, SpeedKStr, SpeedMStr, SpeedGStr, SpeedTStr);
-    session = tr_sessionInit(configDir, true, settings);
+    session = tr_sessionInit(config_dir, true, settings);
     tr_sessionSetRPCCallback(session, on_rpc_callback, nullptr);
-    tr_logAddInfo(fmt::format(_("Loading settings from '{path}'"), fmt::arg("path", configDir)));
-    tr_sessionSaveSettings(session, configDir, settings);
+    tr_logAddInfo(fmt::format(_("Loading settings from '{path}'"), fmt::arg("path", config_dir)));
+    tr_sessionSaveSettings(session, config_dir, settings);
 
     auto sv = std::string_view{};
     (void)tr_variantDictFindStrView(settings, key_pidfile, &sv);
@@ -893,7 +886,7 @@ CLEANUP:
 
     event_base_free(ev_base);
 
-    tr_sessionSaveSettings(mySession, configDir, settings);
+    tr_sessionSaveSettings(mySession, config_dir, settings);
     tr_sessionClose(mySession);
     pumpLogMessages(logfile);
     printf(" done.\n");
@@ -922,12 +915,12 @@ CLEANUP:
 
 static bool init_daemon_data(int argc, char* argv[], struct daemon_data* data, bool* foreground, int* ret)
 {
-    data->configDir = getConfigDir(argc, (char const* const*)argv);
+    data->config_dir = getConfigDir(argc, (char const* const*)argv);
 
     /* load settings from defaults + config file */
     tr_variantInitDict(&data->settings, 0);
     tr_variantDictAddBool(&data->settings, TR_KEY_rpc_enabled, true);
-    bool const loaded = tr_sessionLoadSettings(&data->settings, data->configDir, MyName);
+    bool const loaded = tr_sessionLoadSettings(&data->settings, data->config_dir.c_str(), MyName);
 
     bool dumpSettings;
 

gtk/Application.cc
@@ -1382,7 +1382,7 @@ tr_torrent* Application::Impl::get_first_selected_torrent() const
 
 void Application::Impl::copy_magnet_link_to_clipboard(tr_torrent* tor) const
 {
-    char* magnet = tr_torrentGetMagnetLink(tor);
+    auto const magnet = tr_torrentGetMagnetLink(tor);
     auto const display = wind_->get_display();
     GdkAtom selection;
     Glib::RefPtr<Gtk::Clipboard> clipboard;
@@ -1396,9 +1396,6 @@ void Application::Impl::copy_magnet_link_to_clipboard(tr_torrent* tor) const
     selection = GDK_SELECTION_PRIMARY;
     clipboard = Gtk::Clipboard::get_for_display(display, selection);
     clipboard->set_text(magnet);
-
-    /* cleanup */
-    tr_free(magnet);
 }
 
 void gtr_actions_handler(Glib::ustring const& action_name, gpointer user_data)

gtk/DetailsDialog.cc
@@ -22,7 +22,7 @@
 #include <fmt/format.h>
 
 #include <libtransmission/transmission.h>
-#include <libtransmission/utils.h> /* tr_free */
+#include <libtransmission/utils.h>
 #include <libtransmission/web-utils.h>
 
 #include "Actions.h"
@@ -2299,19 +2299,6 @@ void DetailsDialog::Impl::on_edit_trackers_response(int response, std::shared_pt
     }
 }
 
-namespace
-{
-
-std::string get_editable_tracker_list(tr_torrent const* tor)
-{
-    char* cstr = tr_torrentGetTrackerList(tor);
-    auto str = std::string{ cstr != nullptr ? cstr : "" };
-    tr_free(cstr);
-    return str;
-}
-
-} // namespace
-
 void DetailsDialog::Impl::on_edit_trackers()
 {
     tr_torrent const* tor = tracker_list_get_current_torrent();
@@ -2343,7 +2330,7 @@ void DetailsDialog::Impl::on_edit_trackers()
         t->add_wide_control(row, *l);
 
         auto* w = Gtk::make_managed<Gtk::TextView>();
-        w->get_buffer()->set_text(get_editable_tracker_list(tor));
+        w->get_buffer()->set_text(tr_torrentGetTrackerList(tor));
         auto* fr = Gtk::make_managed<Gtk::Frame>();
         fr->set_shadow_type(Gtk::SHADOW_IN);
         auto* sw = Gtk::make_managed<Gtk::ScrolledWindow>();

gtk/Prefs.cc
@@ -49,9 +49,7 @@ static void tr_prefs_init_defaults(tr_variant* d)
 
     if (dir.empty())
     {
-        auto* const tmp = tr_getDefaultDownloadDir();
-        dir = tmp;
-        tr_free(tmp);
+        dir = tr_getDefaultDownloadDir();
     }
 
     tr_variantDictReserve(d, 31);

gtk/main.cc
@@ -96,9 +96,7 @@ int main(int argc, char** argv)
     /* set up the config dir */
     if (std::empty(config_dir))
     {
-        auto* const default_config_dir = tr_getDefaultConfigDir(AppConfigDirName);
-        config_dir = default_config_dir;
-        tr_free(default_config_dir);
+        config_dir = tr_getDefaultConfigDir(AppConfigDirName);
     }
 
     gtr_pref_init(config_dir);

libtransmission/platform.cc
@@ -110,38 +110,43 @@ static std::string xdgConfigHome()
     return fmt::format("{:s}/.config"sv, getHomeDir());
 }
 
-char* tr_getDefaultConfigDir(char const* appname)
+std::string tr_getDefaultConfigDir(std::string_view appname)
 {
-    if (auto dir = tr_env_get_string("TRANSMISSION_HOME"sv); !std::empty(dir))
+    if (std::empty(appname))
     {
-        return tr_strvDup(dir);
+        appname = "Transmission"sv;
     }
 
-    if (tr_str_is_empty(appname))
+    if (auto dir = tr_env_get_string("TRANSMISSION_HOME"sv); !std::empty(dir))
     {
-        appname = "Transmission";
+        return dir;
     }
 
 #ifdef __APPLE__
 
-    return tr_strvDup(fmt::format("{:s}/Library/Application Support/{:s}"sv, getHomeDir(), appname));
+    return fmt::format("{:s}/Library/Application Support/{:s}"sv, getHomeDir(), appname);
 
 #elif defined(_WIN32)
 
     auto const appdata = win32_get_known_folder(FOLDERID_LocalAppData);
-    return tr_strvDup(fmt::format("{:s}/{:s}"sv, appdata, appname));
+    return fmt::format("{:s}/{:s}"sv, appdata, appname);
 
 #elif defined(__HAIKU__)
 
     char buf[PATH_MAX];
     find_directory(B_USER_SETTINGS_DIRECTORY, -1, true, buf, sizeof(buf));
-    return tr_strvDup(fmt::format("{:s}/{:s}"sv, buf, appname);
+    return fmt::format("{:s}/{:s}"sv, buf, appname);
 
 #else
 
-    return tr_strvDup(fmt::format("{:s}/{:s}"sv, xdgConfigHome(), appname));
+    return fmt::format("{:s}/{:s}"sv, xdgConfigHome(), appname);
 
 #endif
+}
+
+size_t tr_getDefaultConfigDirToBuf(char const* appname, char* buf, size_t buflen)
+{
+    return tr_strvToBuf(tr_getDefaultConfigDir(appname != nullptr ? appname : ""), buf, buflen);
 }
 
 static std::string getXdgEntryFromUserDirs(std::string_view key)
@@ -178,25 +183,30 @@ static std::string getXdgEntryFromUserDirs(std::string_view key)
     return val;
 }
 
-char* tr_getDefaultDownloadDir()
+std::string tr_getDefaultDownloadDir()
 {
-    if (auto const dir = getXdgEntryFromUserDirs("XDG_DOWNLOAD_DIR"sv); !std::empty(dir))
+    if (auto dir = getXdgEntryFromUserDirs("XDG_DOWNLOAD_DIR"sv); !std::empty(dir))
     {
-        return tr_strvDup(dir);
+        return dir;
     }
 
 #ifdef _WIN32
     if (auto dir = win32_get_known_folder(FOLDERID_Downloads); !std::empty(dir))
     {
-        return tr_strvDup(dir);
+        return dir;
     }
 #endif
 
 #ifdef __HAIKU__
-    return tr_strvDup(fmt::format("{:s}/Desktop"sv, getHomeDir()));
+    return fmt::format("{:s}/Desktop"sv, getHomeDir());
 #endif
 
-    return tr_strvDup(fmt::format("{:s}/Downloads"sv, getHomeDir()));
+    return fmt::format("{:s}/Downloads"sv, getHomeDir());
+}
+
+size_t tr_getDefaultDownloadDirToBuf(char* buf, size_t buflen)
+{
+    return tr_strvToBuf(tr_getDefaultDownloadDir(), buf, buflen);
 }
 
 /***

libtransmission/session.cc
@@ -296,7 +296,7 @@ tr_session::PublicAddressResult tr_session::publicAddress(tr_address_type type) 
 
 void tr_sessionGetDefaultSettings(tr_variant* setme_dictionary)
 {
-    auto* const download_dir = tr_getDefaultDownloadDir();
+    auto const download_dir = tr_getDefaultDownloadDir();
 
     auto* const d = setme_dictionary;
     TR_ASSERT(tr_variantIsDict(d));
@@ -376,8 +376,6 @@ void tr_sessionGetDefaultSettings(tr_variant* setme_dictionary)
     tr_variantDictAddBool(d, TR_KEY_anti_brute_force_enabled, true);
     tr_variantDictAddStrView(d, TR_KEY_announce_ip, "");
     tr_variantDictAddBool(d, TR_KEY_announce_ip_enabled, false);
-
-    tr_free(download_dir);
 }
 
 void tr_sessionGetSettings(tr_session const* s, tr_variant* setme_dictionary)
@@ -469,9 +467,8 @@ static void getSettingsFilename(tr_pathbuf& setme, char const* config_dir, char 
         return;
     }
 
-    auto* const default_config_dir = tr_getDefaultConfigDir(appname);
+    auto const default_config_dir = tr_getDefaultConfigDir(appname);
     setme.assign(std::string_view{ default_config_dir }, "/settings.json"sv);
-    tr_free(default_config_dir);
 }
 
 bool tr_sessionLoadSettings(tr_variant* dict, char const* config_dir, char const* appName)

commit 8a28ab140b6b965a30409ec8c3f773aeed5b7e5b

fix: restore tr_optind in all getConfigDir branches (#6920)
When the --config-dir/-g option was passed, this bug caused all options
preceeding it on the command line to be ignored.

Also remove the superfluous break statement.

Regression introduced by 726c93208ab0c47c267b9baaf4b8f43759d7a1d7.

cli/cli.cc
@@ -167,8 +167,8 @@ void onTorrentFileDownloaded(tr_web::FetchResponse const& response)
     {
         if (c == 'g')
         {
+            tr_optind = ind;
             return my_optarg;
-            break;
         }
     }
 