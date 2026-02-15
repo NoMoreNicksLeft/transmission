NoMoreNicksLeft/transmission

commit aacfad1b4bc3ea076accb3520bb9e1a95eda183d

perf: faster arrow key navigation in torrent list (#8315) (#8323)
* perf: when selection changes, only update the rows whose selection changed

previously we were updating all rows

* perf: coalesce pending reloads in TorrentTableView

* refactor: return a copy rather thabn a mutable index

* refactor: move the assignment out of the if statement

* make symmetricDifference an NSIndexSet method

macosx/TorrentTableView.mm
@@ -28,6 +28,29 @@ static CGFloat const kErrorImageSize = 20.0;
 
 static NSTimeInterval const kToggleProgressSeconds = 0.175;
 
+@interface NSIndexSet (Transmission)
+- (NSIndexSet*)symmetricDifference:(NSIndexSet*)otherSet;
+@end
+
+@implementation NSIndexSet (Transmission)
+
+- (NSIndexSet*)symmetricDifference:(NSIndexSet*)otherSet
+{
+    NSMutableIndexSet* result = [self mutableCopy];
+    [result addIndexes:otherSet];
+
+    [self enumerateIndexesUsingBlock:^(NSUInteger idx, BOOL*) {
+        if ([otherSet containsIndex:idx])
+        {
+            [result removeIndex:idx];
+        }
+    }];
+
+    return [result copy];
+}
+
+@end
+
 @interface TorrentTableView ()
 
 @property(nonatomic) IBOutlet Controller* fController;
@@ -48,6 +71,8 @@ static NSTimeInterval const kToggleProgressSeconds = 0.175;
 @property(nonatomic) NSView* fPositioningView;
 
 @property(nonatomic) NSDictionary* fHoverEventDict;
+
+@property(nonatomic) NSMutableIndexSet* fPendingSelectionReloadRows;
 
 @end
 
@@ -430,8 +455,32 @@ static NSTimeInterval const kToggleProgressSeconds = 0.175;
 
 - (void)outlineViewSelectionDidChange:(NSNotification*)notification
 {
-    self.fSelectedRowIndexes = self.selectedRowIndexes;
-    [self reloadVisibleRows];
+    NSIndexSet* oldSelection = self.fSelectedRowIndexes ?: [NSIndexSet indexSet];
+    NSIndexSet* newSelection = self.selectedRowIndexes;
+    self.fSelectedRowIndexes = newSelection;
+
+    NSIndexSet* changedRows = [oldSelection symmetricDifference:newSelection];
+    if (changedRows.count > 0)
+    {
+        if (!self.fPendingSelectionReloadRows)
+        {
+            self.fPendingSelectionReloadRows = [[NSMutableIndexSet alloc] init];
+            [self performSelector:@selector(flushSelectionReload) withObject:nil afterDelay:0 inModes:@[ NSRunLoopCommonModes ]];
+        }
+
+        [self.fPendingSelectionReloadRows addIndexes:changedRows];
+    }
+}
+
+- (void)flushSelectionReload
+{
+    NSMutableIndexSet* rows = self.fPendingSelectionReloadRows;
+    self.fPendingSelectionReloadRows = nil;
+
+    if (rows.count > 0)
+    {
+        [self reloadDataForRowIndexes:rows columnIndexes:[NSIndexSet indexSetWithIndex:0]];
+    }
 }
 
 - (void)outlineViewItemDidExpand:(NSNotification*)notification

commit 0cb08e38ba402e4e01da0ac698bc6aaacd2796b7

fix: UI update regression (#8419) (#8517)
introduced last week by aacfad1b4

macosx/TorrentTableView.mm
@@ -477,6 +477,8 @@ static NSTimeInterval const kToggleProgressSeconds = 0.175;
     NSMutableIndexSet* rows = self.fPendingSelectionReloadRows;
     self.fPendingSelectionReloadRows = nil;
 
+    NSInteger const numberOfRows = self.numberOfRows;
+    [rows removeIndexesInRange:NSMakeRange(numberOfRows, NSIntegerMax - numberOfRows)];
     if (rows.count > 0)
     {
         [self reloadDataForRowIndexes:rows columnIndexes:[NSIndexSet indexSetWithIndex:0]];