f5f0c0c8febf296a52828e4e22a106d79e2bb68f
John Oyler <john.m.oyler@gmail.com>
2026-04-11 21:03:03 -0500

    fix: increase sync_handlers capacity from 20 to 26
    Adding dht_add_node exceeded the small::max_size_map compile-time
    capacity, causing std::length_error during static initialization and
    an immediate crash on launch.

--- libtransmission/rpcimpl.cc
+++ libtransmission/rpcimpl.cc
@@ -2788,7 +2788,7 @@ using SessionAccessors = std::pair<SessionGetter, SessionSetter>;
 
 using SyncHandler = std::pair<JsonRpc::Error::Code, std::string> (*)(tr_session*, tr_variant::Map const&, tr_variant::Map&);
 
-auto const sync_handlers = small::max_size_map<tr_quark, std::pair<SyncHandler, bool /*has_side_effects*/>, 20U>{ {
+auto const sync_handlers = small::max_size_map<tr_quark, std::pair<SyncHandler, bool /*has_side_effects*/>, 26U>{ {
     { TR_KEY_free_space, { freeSpace, false } },
     { TR_KEY_group_get, { groupGet, false } },
     { TR_KEY_group_set, { groupSet, true } },
